592b1b4730c7f0eee16a66e3cbf274c9ed80cffc
stysiak <83347830+stysiak@users.noreply.github.com>
2024-12-03 11:12:51 +0100

    plik header

---
+++ Biblioteka/Biblioteka/header.h
@@ -0,0 +1,7 @@
+#pragma once
+// wszystkie #include
+using namespace std;
+//wszystkie nazwy klas
+//plik header do wszystkich nag³ówków
+//jesli klasa korzysta z obiektow innych klas, to nag³ówek te klasy dodaæ do nag³ówku klasy u¿ywaj¹cej
+
