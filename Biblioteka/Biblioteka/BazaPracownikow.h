68ba1a03d1a60173185d93fd0011665dbc359776
jagnaki <148761226+jagnaki@users.noreply.github.com>
2024-12-17 11:06:40 +0100

    Aktualizacja logowania w BaziePracownikow i klasy Pracownik

--- Biblioteka/Biblioteka/BazaPracownikow.h
+++ Biblioteka/Biblioteka/BazaPracownikow.h
@@ -6,5 +6,22 @@ private:
 	vector<Pracownik> listaPracownikow;
 public:
 	BazaPracownikow();
-	bool logowanie(Pracownik p);
+	bool logowanie(Pracownik p) {
+		string wpisanyLogin, wpisaneHaslo;
+
+		cout << "Podaj login: ";
+		cin >> wpisanyLogin;
+
+		cout << "Podaj haslo: ";
+		cin >> wpisaneHaslo;
+		for (const auto& pracownik : listaPracownikow) {
+			if (pracownik.getLogin() == wpisanyLogin && pracownik.getHaslo() == wpisaneHaslo) {
+				cout << pracownik.getLogin() << ": logowanie pomyslne\n";
+				return true;
+			}
+			cout << "Blad logowania!\n";
+			return false;
+		}
+	}
+	//dodajPracownika?
 };
--- Biblioteka/Biblioteka/Pracownik.h
+++ Biblioteka/Biblioteka/Pracownik.h
@@ -7,9 +7,13 @@ private:
 	string imie;
 	string nazwisko;
 	float pensja;
+	string login;
+	string haslo;
 public:
 	Pracownik();
 	void sprawdzenieKonta(KontoCzytelnika);
 	void przyjmijKaucje(KontoCzytelnika, Egzemplarz);
+	string getLogin() const;
+	string getHaslo() const;
 
 };
