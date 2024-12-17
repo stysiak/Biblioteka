da22d3a0fca3257bc57437c011ce55b54847eff5
jagnaki <148761226+jagnaki@users.noreply.github.com>
2024-12-17 10:54:36 +0100

    Zaktualizowano tworzenieKonta i usuniecieKonta w BazaCzytelnikow

--- Biblioteka/Biblioteka/BazaCzytelnikow.h
+++ Biblioteka/Biblioteka/BazaCzytelnikow.h
@@ -6,7 +6,28 @@ private:
 	vector<KontoCzytelnika> listaCzytelnikow;
 public:
 	BazaCzytelnikow();
-	void tworzenieKonta(KontoCzytelnika czytelnik);
-	void usuniecieKonta(KontoCzytelnika czytelnik);
-	bool sprawdzenieKOnta(KontoCzytelnika czytelnik);
+	void tworzenieKonta(KontoCzytelnika czytelnik) {
+		if (!sprawdzenieKonta(czytelnik)) {
+			listaCzytelnikow.push_back(czytelnik);
+			cout << "Utworzono konto czytelnika\n";
+		}
+		else {
+			cout << "Konto czytelnika juz istnieje\n";
+		}
+	}
+	void usuniecieKonta(KontoCzytelnika czytelnik) {
+		auto it = find(listaCzytelnikow.begin(), listaCzytelnikow.end(), czytelnik);
+
+		if (it != listaCzytelnikow.end()) {
+			listaCzytelnikow.erase(it);
+			cout << "Usunieto konto czytelnika\n";
+		}
+		else {
+			cout << "Nie ma takiego konta\n";
+		}
+
+	}
+	bool sprawdzenieKonta(KontoCzytelnika czytelnik) {
+		
+	}
 };
